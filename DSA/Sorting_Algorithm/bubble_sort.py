11d0de88cf4fd6c909f7e92076935871d637f832
bobby <blahblah@gmail>
2024-12-28 20:35:25 +1100

    implemented bubble sort I

--- DSA/Sorting_Algorithm/bubble_sort.py
+++ DSA/Sorting_Algorithm/bubble_sort.py
@@ -15,7 +15,7 @@ def bubble_sort_I(array: list) -> None:
 
     for i in range(n - 1):
         for j in range(mark):
-            if array[j] + array[j + 1]:
+            if array[j] > array[j + 1]:
                 swap(array, j, j + 1)
             
         mark -= 1
